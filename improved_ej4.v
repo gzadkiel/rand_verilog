21548772c395f2dd1993de44e6948be8e678dc66
gm <tickimarti@hotmail.com>
2023-03-01 22:13:59 -0300

    Revisar porque se cae a pedazos.

--- improved_ej4.v
+++ improved_ej4.v
@@ -1,13 +1,14 @@
-`define NBinput     32
-`define NBoutput    64
+// General parameters
+`define NBinput     8
+`define NBoutput    8
 `define NTAPS       4
 
 // Coefficients
-`define b0          1
-`define b1          -1
-`define b2          1
-`define b3          1
-`define a0          1
+`define b0      1
+`define b1      -1
+`define b2      1
+`define a0      1
+`define b3      1
 
 module improved_ej4
     #(
@@ -30,16 +31,16 @@ module improved_ej4
     );
 
 // Product wires
-wire signed [NBoutput - 1 : 0] cable_b[NTAPS];
-wire signed [NBoutput - 1 : 0] cable_a[NTAPS-1];
+wire signed [NBoutput - 1 : 0] cable_b [NTAPS     : 0];
+wire signed [NBoutput - 1 : 0] cable_a [NTAPS - 1 : 0];
 
 // Registers
-reg signed [NBoutput - 1 : 0] z[NTAPS*2];
+reg signed [NBoutput - 1 : 0] z [NTAPS * 2 : 0];
 
 // Circular buffer 
 always @(posedge clk or negedge rst_n) begin
     if (!rst_n) begin
-        z <= {NBoutput{1'b0}};
+        z [NBoutput - 1 : 0] [NTAPS * 2 : 0]<= {NBoutput{1'b0}};
     end
     else begin
         // Pipeline X registers
@@ -59,3 +60,5 @@ assign cable_a[0] = z[NBOUTPUT*1-1:NBOUTPUT+3] >>> 2;
 assign cable_a[1] = z[NBOUTPUT*2-1:NBOUTPUT*1+7] >>> 4;
 
 assign Y = {cable_b, cable_a};
+
+endmodule
